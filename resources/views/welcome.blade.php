ee0684858737387ff1a11e3b7d830306b125b4df
Anderson Coimbra <andersonmegax@hotmail.com>
2021-02-08 01:33:11 -0300

    Teste de deploy no azure

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Coimbra
                 </div>
 
                 <div class="links">
